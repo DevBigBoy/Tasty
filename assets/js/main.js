2fca0fd7fcaace940d3f399ed9636e7a4e8d30e7
DevMohamedElshazly <moshazly416@gmail.com>
2022-06-02 04:30:00 +0200

    add-header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,62 @@
 /*==================== SHOW MENU ====================*/
+const navToggle = document.getElementById("nav-toggle"),
+  navMenu = document.getElementById("nav-menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show-menu");
+});
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((item) => item.addEventListener("click", linkAction));
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
 
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
 /*==================== DARK LIGHT THEME ====================*/
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
